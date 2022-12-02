26d45e11280df4d95d4a2756897fd0630cc40ac2
Mael Brocher <mael.brocher@epitech.eu>
2022-12-02 09:25:45 +0100

    Day1 done

--- Day1/code_day_1.py
+++ Day1/code_day_1.py
@@ -1,4 +1,9 @@
 def main():
+    f = open("input.sql",'r').read()
+    elves = [ sum([int(c) for c in cal.split('\n')]) for cal in f.split('\n\n')]
+    print("Part 1 = {}".format(max(elves)))
+    elves.sort()
+    print("Part 2 = {}".format(sum(elves[-3:])))    
     return
 
 if __name__ == '__main__':
