9dbcd2cd83758b9c3767aaac600a7b43e1cd0c35
Mael Brocher <mael.brocher@epitech.eu>
2022-12-05 11:34:50 +0100

    Day3 (laid sa mère)

--- Day2/code_day_2.py
+++ Day2/code_day_2.py
@@ -10,10 +10,7 @@ def fuse(Opp, Me):
 
 def main():
     outcome = {fuse(O_Cissors, M_Cissors):3+3, fuse(O_Paper, M_Paper):2+3, fuse(O_Rock, M_Rock):1+3,fuse(O_Cissors, M_Rock):6+1, fuse(O_Paper, M_Cissors):6+3, fuse(O_Rock, M_Paper):6+2, fuse(O_Cissors, M_Paper):0+2, fuse(O_Paper, M_Rock):0+1, fuse(O_Rock, M_Cissors):0+3}
-    score = 0
-    for l in open("input.sql", "r").readlines():
-        score += outcome[l]
-    print("Part 1 : {}".format(score))
+    print("Part 1 : {}".format( sum(outcome[l] for l in open("input.sql", "r").readlines())))
 
     win = {O_Rock : M_Paper, O_Paper : M_Cissors, O_Cissors : M_Rock}
     lose = {O_Rock : M_Cissors, O_Paper : M_Rock, O_Cissors : M_Paper}
--- Day3/code_day_3.py
+++ Day3/code_day_3.py
@@ -1,5 +1,27 @@
-def main():
-    return
+def getscore():
+    score = {}
+    for i in range(97, 123):
+        score[chr(i)] = i - 96
+    for i in range(65, 91):
+        score[chr(i)] = i - 65+27
+    return score
 
+def betterSplit(a):
+    b = []
+    tmp = []
+    for y,s in enumerate(a):
+        if y%3 == 0 and y != 0:
+            b.append(tmp)
+            tmp = []
+        tmp.append(s.strip('\n'))
+    b.append(tmp)
+    return b
+
+def main():
+    score = getscore()
+    bags = sum([score[list(set(l[:len(l)//2]).intersection(set(l[len(l)//2:])))[0]] for l in open('input.sql').readlines()])
+    print("Part 1 : {}".format(bags))
+    bags = sum([score[list(set(b[0]).intersection(b[1]).intersection(b[2]))[0]] for b in betterSplit(open('input.sql').readlines())])
+    print("Part 2 : {}".format(bags))
 if __name__ == '__main__':
     main()
