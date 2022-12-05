689260b5ebf538a74d53042d638e1a2f3d7d6719
Mael Brocher <mael.brocher@epitech.eu>
2022-12-05 14:35:51 +0100

    bettersplit is better (merci tatave)

--- Day3/code_day_3.py
+++ Day3/code_day_3.py
@@ -7,21 +7,13 @@ def getscore():
     return score
 
 def betterSplit(a):
-    b = []
-    tmp = []
-    for y,s in enumerate(a):
-        if y%3 == 0 and y != 0:
-            b.append(tmp)
-            tmp = []
-        tmp.append(s.strip('\n'))
-    b.append(tmp)
-    return b
+    return [a[i*3:i*3+3] for i in range(len(a)//3)]
 
 def main():
     score = getscore()
     bags = sum([score[list(set(l[:len(l)//2]).intersection(set(l[len(l)//2:])))[0]] for l in open('input.sql').readlines()])
     print("Part 1 : {}".format(bags))
-    bags = sum([score[list(set(b[0]).intersection(b[1]).intersection(b[2]))[0]] for b in betterSplit(open('input.sql').readlines())])
+    bags = sum([score[list(set(b[0].strip('\n')).intersection(b[1].strip('\n')).intersection(b[2].strip('\n')))[0]] for b in betterSplit(open('input.sql').readlines())])
     print("Part 2 : {}".format(bags))
 if __name__ == '__main__':
     main()
